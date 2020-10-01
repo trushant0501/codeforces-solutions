f8db88690bc1d0332f5a78adaa8daa25adfd38d5
het262 <71756306+het262@users.noreply.github.com>
2020-10-02 03:21:47 +0530

    Update 443A - Anton and Letters.cpp

--- 443A - Anton and Letters.cpp
+++ 443A - Anton and Letters.cpp
@@ -1,21 +1,51 @@
-#include <iostream>
-#include <algorithm>
-#include<string>
-#include<vector>
-#include<stdio.h>
-using namespace std;
-
-int main() {
-	// your code goes here
-	char x;
-	bool a[26]={0};
-	int c=0;
-	while(scanf("%c",&x)==1&&x!='\0'&&x!='\n'&&x!='}'){
-		if((x!=' '&&x!=','&&x!='{'&&x!='}'&&a[int(tolower(x))-97]!=1)){
-			a[int(tolower(x))-97]=1;
-			c++;
+	#include<bits/stdc++.h>
+		using namespace std;
+		#define FAST ios_base::sync_with_stdio(false), cin.tie(0), cout.tie(0);
+		#include <ext/pb_ds/assoc_container.hpp> 
+		#include <ext/pb_ds/tree_policy.hpp> 
+		using namespace __gnu_pbds; 
+		#define ordered_set tree<int, null_type,less<int>, rb_tree_tag,tree_order_statistics_node_update>
+		 
+		#define ff first
+		#define ss second
+		#define mp make_pair
+		 
+		#define pb push_back
+		#define endl "\n"
+		 
+		#define int long long int
+		#define double long double
+		 
+		#define bp __builtin_popcountllm
+		#define inf 1e16
+		const int N=1e5+5;
+		const int M=105;
+		const int MOD=1e9+7;
+ 
+		
+		void solve()
+		{	
+		   map<int,int> mm;
+	       string s;
+	       getline(cin,s);
+	       int n=s.size();
+	       for(int i=1;i<n;i+=3){
+	       		if(s[i]>='a' && s[i]<='z')
+	       		mm[s[i]-'a']++;
+	       }
+	       
+	       int res=mm.size();
+	       cout<<res<<endl;
 		}
-	}
-	cout<<c<<endl;
-	return 0;
-}
+ 
+		signed main()  
+		{
+			FAST
+			int t=1;
+			//cin>>t;
+			while(t--)
+			{
+				solve();
+				
+			}
+		}
